4ae753ade553f304392be53a073f8170d47063b8
Derrick Shema <61860253+derrick-shema@users.noreply.github.com>
2024-01-12 17:54:41 -0600

    staff domain

---
+++ src/domain/user/entities/staff.ts
@@ -0,0 +1,46 @@
+import Email from "../value-objects/user-email";
+import UserId from "../value-objects/user-id";
+import HashedPassword from "../value-objects/user-password";
+import User from "./user";
+
+export default class Staff extends User {
+  getRole(){
+    return this.role;
+  }
+
+  getTitle() {
+    return this.title;
+  }
+
+  constructor(
+    id: UserId,
+    firstName: string,
+    lastName: string,
+    email: Email,
+    hashedAndSaltedPassword: HashedPassword,
+    private role: string,
+    private title: string
+  ){
+    super(id, firstName, lastName, email, hashedAndSaltedPassword)
+  }
+
+  static create(
+    id: UserId,
+    firstName: string,
+    lastName: string,
+    email: Email,
+    hashedAndSaltedPassword: HashedPassword,
+    role: string,
+    title: string
+  ){
+    return new Staff(
+      id,
+      firstName,
+      lastName,
+      email,
+      hashedAndSaltedPassword,
+      role,
+      title
+    )
+  }
+}
